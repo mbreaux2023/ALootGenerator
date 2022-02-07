8af099f2efa1f7fff1dccccf31f1d2ab62eb31ef
mbreaux2023 <mbreaux2023@chadwickschool.org>
2022-02-06 17:20:13 -0800

    finish get valid int

--- TestGetValidInt.cs
+++ TestGetValidInt.cs
@@ -9,6 +9,7 @@ namespace ALootGenerator
             Console.WriteLine("the method should display 'How many options would you like?'");
             Console.WriteLine("type '-'. You should see 'invalid!'");
             Console.WriteLine("Then type '-4'. You should see 'invalid!'");
+            
             // TODO(jcollard 2022-02-04): Instruct the user to enter '3'. 
             int result = Program.GetValidInt("how many options would you like");
             
@@ -24,6 +25,10 @@ namespace ALootGenerator
             // * Test 1 more valid input.
 
             result = Program.GetValidInt("How old are you?");
+
+            Console.WriteLine("the Method should display 'How many options would you like?'");
+            Console.WriteLine("type 'banana'. You should see 'invalid!'");
+            Console.WriteLine("then type '-12'. You should see invalid.");
 
             return true;
         }
